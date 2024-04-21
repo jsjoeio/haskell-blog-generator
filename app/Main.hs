4a459bdfec76bc03b3b3cc8b3340d45ebd936437
Joe Previte <jjprevite@gmail.com>
2024-04-21 22:35:12 +0000

    feat: add code for 3.2
    This adds code plus some notes

--- app/Main.hs
+++ app/Main.hs
@@ -4,19 +4,29 @@ main :: IO ()
 main = putStrLn myhtml
 
 myhtml :: String
-myhtml = makeHtml "Hello title" "Hello, world!"
+myhtml = makeHtml "Hello title" (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
 
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title)) <> body_ content
 
 html_ :: String -> String
-html_ content = "<html>" <> content <> "</html>"
+html_ = el "html"
 
 body_ :: String -> String
-body_ content = "<body>" <> content <> "</body>"
+body_ = el "body"
 
 head_ :: String -> String
-head_ content = "<head>" <> content <> "</head>"
+head_ = el "head"
 
 title_ :: String -> String
-title_ content = "<title>" <> content <> "</title>"
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
