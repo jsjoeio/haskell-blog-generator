19df4ef1888fd90f1daae5132d9f5ac3579bdf69
Joe Previte <jjprevite@gmail.com>
2024-04-21 22:25:10 +0000

    feat: add code for 3.1
    This completes the exercises for 3.1. Things to note:
    - how it's like a function call without the parens
    
    helloWorld("joe") in JS/TS
    
    vs
    
    helloWorld "joe" in Haskell

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,22 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "<html><body>Hello, world!</body></html>"
+main = putStrLn myhtml
 
+myhtml :: String
+myhtml = makeHtml "Hello title" "Hello, world!"
+
+makeHtml :: String -> String -> String
+makeHtml title content = html_ (head_ (title_ title)) <> body_ content
+
+html_ :: String -> String
+html_ content = "<html>" <> content <> "</html>"
+
+body_ :: String -> String
+body_ content = "<body>" <> content <> "</body>"
+
+head_ :: String -> String
+head_ content = "<head>" <> content <> "</head>"
+
+title_ :: String -> String
+title_ content = "<title>" <> content <> "</title>"
